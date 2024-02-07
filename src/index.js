2c45099eabbebfe470a001152c44bdb216410eb5
Anna Mendrukh <amendrukh@gmail.com>
2024-02-07 17:32:55 +0200

    Bug with position images into section education__journey has been fixed.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 import html from "./index.html";
 import "./sass/styles.scss"
 
-console.log(123555555)
+
+/*section education*/
+const boyIcon = document.querySelector(".boy-icon");
+const girlIcon = document.querySelector(".girl-icon");
+const bgBlur = document.querySelector(".solution__bg-blur");
+const list = document.querySelector(".education__list");
+
+function updateTopValue() {
+    const boyIconHeight = boyIcon.offsetHeight;
+    const girlIconHeight = girlIcon.offsetHeight;
+    const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    const listHeight = list.getBoundingClientRect().height;
+    if (screenWidth < "1200") {
+        return bgBlur.style.top = (boyIconHeight - girlIconHeight) + "px";
+     }
+    else {
+
+        return bgBlur.style.top = -(listHeight/3) + "px";
+     }
+
+
+}
+
+window.addEventListener('load', updateTopValue);
+window.addEventListener('resize', updateTopValue);
 
